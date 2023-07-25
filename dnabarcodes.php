8eb4cfda4ba6ed6c5da226d36c292dc5d9c5108c
zygoballus <119077481+zygoballus@users.noreply.github.com>
2023-07-25 12:52:53 -0400

    Handle iNat URLs too

--- dnabarcodes.php
+++ dnabarcodes.php
@@ -81,34 +81,38 @@ function iNat_auth_request( $app_id, $app_secret, $username, $password, $url = '
 // See if form was submitted.
 if ( $_POST ) {
 	if ( isset( $_POST['observation'] )
-		&& is_numeric( $_POST['observation'] )
 		&& $_POST['gene']
 		&& isset( $_POST['barcode'] )
 		&& strlen( $_POST['barcode'] ) > 10
 	) {
-		$postData['observation_field_value'] = [];
-		$postData['observation_field_value']['observation_id'] = intval( $_POST['observation'] );
-		$postData['observation_field_value']['value'] = $_POST['barcode'];
-		switch ( $_POST['gene'] ) {
-			case 'COI':
-				$postData['observation_field_value']['observation_field_id'] = 824;
-				break;
-			case 'ITS':
-				$postData['observation_field_value']['observation_field_id'] = 2330;
-				break;
-			case '18S':
-				$postData['observation_field_value']['observation_field_id'] = 14900;
-				break;
-		}
-		$postData = json_encode( $postData );
-  		$response = iNat_auth_request( $app_id, $app_secret, $username, $password );
-		if ( $response ) {
-			$token = $response['access_token'];
-			$url = $inatapi . 'observation_field_values';
-			$response = make_curl_request( $url, $token, $postData );
-			if ( $response ) {
-				$updated = true;
+		if ( preg_match( '/\d+/', $_POST['observation'], $matches ) ) {
+			$observationid = $matches[0];
+			$postData['observation_field_value'] = [];
+			$postData['observation_field_value']['observation_id'] = intval( $observationid );
+			$postData['observation_field_value']['value'] = $_POST['barcode'];
+			switch ( $_POST['gene'] ) {
+				case 'COI':
+					$postData['observation_field_value']['observation_field_id'] = 824;
+					break;
+				case 'ITS':
+					$postData['observation_field_value']['observation_field_id'] = 2330;
+					break;
+				case '18S':
+					$postData['observation_field_value']['observation_field_id'] = 14900;
+					break;
 			}
+			$postData = json_encode( $postData );
+			$response = iNat_auth_request( $app_id, $app_secret, $username, $password );
+			if ( $response ) {
+				$token = $response['access_token'];
+				$url = $inatapi . 'observation_field_values';
+				$response = make_curl_request( $url, $token, $postData );
+				if ( $response ) {
+					$updated = true;
+				}
+			}
+		} else {
+			$errors[] = 'Invalid value for iNaturalist observation.';
 		}
 	}
 }
